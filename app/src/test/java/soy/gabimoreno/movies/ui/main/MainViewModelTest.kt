c2d15d38d2157ef194c2c708bd0c9b6d6c7f241c
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-03-04 06:36:40 +0100

    MainViewModelTest completed

--- app/src/test/java/soy/gabimoreno/movies/ui/main/MainViewModelTest.kt
+++ app/src/test/java/soy/gabimoreno/movies/ui/main/MainViewModelTest.kt
@@ -3,7 +3,9 @@ package soy.gabimoreno.movies.ui.main
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -12,6 +14,7 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import soy.gabimoreno.movies.common.event.Event
 import soy.gabimoreno.movies.domain.Movie
+import soy.gabimoreno.movies.testshared.mockedMovie
 import soy.gabimoreno.movies.usecases.GetPopularMovies
 
 @RunWith(MockitoJUnitRunner::class)
@@ -25,6 +28,9 @@ class MainViewModelTest {
 
     @Mock
     lateinit var moviesObserver: Observer<List<Movie>>
+
+    @Mock
+    lateinit var loadingObserver: Observer<Boolean>
 
     @Mock
     lateinit var requestLocationPermissionObserver: Observer<Event<Unit>>
@@ -44,5 +50,31 @@ class MainViewModelTest {
         vm.requestLocationPermission.observeForever(requestLocationPermissionObserver)
 
         verify(requestLocationPermissionObserver).onChanged(vm.requestLocationPermission.value)
+    }
+
+    @Test
+    fun `After requesting the permission, loading is shown`() {
+        runBlocking {
+            val movies = listOf(mockedMovie.copy(id = 1))
+            whenever(getPopularMovies.invoke()).thenReturn(movies)
+            vm.loading.observeForever(loadingObserver)
+
+            vm.onCoarsePermissionRequested()
+
+            verify(loadingObserver).onChanged(vm.loading.value)
+        }
+    }
+
+    @Test
+    fun `After requesting the permission, getPopularMovies() is called`() {
+        runBlocking {
+            val movies = listOf(mockedMovie.copy(id = 1))
+            whenever(getPopularMovies.invoke()).thenReturn(movies)
+            vm.movies.observeForever(moviesObserver)
+
+            vm.onCoarsePermissionRequested()
+
+            verify(moviesObserver).onChanged(vm.movies.value)
+        }
     }
 }
