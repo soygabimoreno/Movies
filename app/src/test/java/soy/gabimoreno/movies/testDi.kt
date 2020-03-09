3c1725a17150944a8bb2ef80805d26cf77769b9a
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-03-09 06:56:12 +0100

    MainIntegrationTests implemented

--- app/src/test/java/soy/gabimoreno/movies/testDi.kt
+++ app/src/test/java/soy/gabimoreno/movies/testDi.kt
@@ -1,21 +1,64 @@
 package soy.gabimoreno.movies
 
+import kotlinx.coroutines.Dispatchers
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import soy.gabimoreno.movies.data.PermissionChecker
-import soy.gabimoreno.movies.data.repository.RegionRepository
+import soy.gabimoreno.movies.data.source.LocalDataSource
 import soy.gabimoreno.movies.data.source.LocationDataSource
 import soy.gabimoreno.movies.data.source.RemoteDataSource
+import soy.gabimoreno.movies.domain.Keys
 import soy.gabimoreno.movies.domain.Movie
 import soy.gabimoreno.movies.testshared.mockedMovie
 
-class FakeRemoteDataSource : RemoteDataSource {
-    override suspend fun getPopularMovies(apiKey: String, region: String): List<Movie> {
-        return listOf(
-            mockedMovie.copy(1),
-            mockedMovie.copy(2),
-            mockedMovie.copy(3),
-            mockedMovie.copy(4)
-        )
+fun initMockedDi(vararg modules: Module) {
+    startKoin {
+        modules(listOf(mockedAppModule, dataModule) + modules)
     }
+}
+
+private val mockedAppModule = module {
+    single(named(Keys.API_KEY)) { "123456" }
+    factory<LocalDataSource> { FakeLocalDataSource() }
+    factory<RemoteDataSource> { FakeRemoteDataSource() }
+    factory<LocationDataSource> { FakeLocationDataSource() }
+    factory<PermissionChecker> { FakePermissionChecker() }
+    single { Dispatchers.Unconfined }
+}
+
+val defaultFakeMovies = listOf(
+    mockedMovie.copy(1),
+    mockedMovie.copy(2),
+    mockedMovie.copy(3),
+    mockedMovie.copy(4)
+)
+
+class FakeLocalDataSource : LocalDataSource {
+
+    var movies: List<Movie> = emptyList()
+
+    override suspend fun isEmpty() = movies.isEmpty()
+
+    override suspend fun getPopularMovies(): List<Movie> = movies
+
+    override suspend fun saveMovies(movies: List<Movie>) {
+        this.movies = movies
+    }
+
+    override suspend fun findById(id: Int): Movie = movies.first { it.id == id }
+
+    override suspend fun update(movie: Movie) {
+        movies = movies.filterNot { it.id == movie.id } + movie
+    }
+}
+
+class FakeRemoteDataSource : RemoteDataSource {
+
+    var movies: List<Movie> = defaultFakeMovies
+
+    override suspend fun getPopularMovies(apiKey: String, region: String): List<Movie> = movies
 }
 
 class FakeLocationDataSource : LocationDataSource {
--- app/src/test/java/soy/gabimoreno/movies/ui/main/MainIntegrationTests.kt
+++ app/src/test/java/soy/gabimoreno/movies/ui/main/MainIntegrationTests.kt
@@ -1,15 +1,33 @@
 package soy.gabimoreno.movies.ui.main
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import soy.gabimoreno.movies.FakeLocalDataSource
+import soy.gabimoreno.movies.data.source.LocalDataSource
+import soy.gabimoreno.movies.defaultFakeMovies
+import soy.gabimoreno.movies.domain.Movie
+import soy.gabimoreno.movies.initMockedDi
+import soy.gabimoreno.movies.testshared.mockedMovie
 import soy.gabimoreno.movies.usecases.GetPopularMovies
 
 @RunWith(MockitoJUnitRunner::class)
 class MainIntegrationTests : AutoCloseKoinTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var moviesObserver: Observer<List<Movie>>
 
     private lateinit var vm: MainViewModel
 
@@ -20,7 +38,28 @@ class MainIntegrationTests : AutoCloseKoinTest() {
             factory { GetPopularMovies(get()) }
         }
 
-//        initMockedDi(vmModule)
+        initMockedDi(vmModule)
         vm = get()
+    }
+
+    @Test
+    fun `Data is loaded from server when local data source is empty`() {
+        vm.movies.observeForever(moviesObserver)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(moviesObserver).onChanged(defaultFakeMovies)
+    }
+
+    @Test
+    fun `Data is loaded from local source when available`() {
+        val fakeLocalMovies = listOf(mockedMovie.copy(id = 10), mockedMovie.copy(id = 11))
+        val localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = fakeLocalMovies
+        vm.movies.observeForever(moviesObserver)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(moviesObserver).onChanged(fakeLocalMovies)
     }
 }
