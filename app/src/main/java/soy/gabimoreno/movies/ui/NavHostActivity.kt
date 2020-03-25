801be3bc87ac9fccedee7d506c4d5d1e61ae2917
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-03-25 07:18:37 +0100

    Resting KEYGUARD_SERVICE

--- app/src/main/java/soy/gabimoreno/movies/ui/NavHostActivity.kt
+++ app/src/main/java/soy/gabimoreno/movies/ui/NavHostActivity.kt
@@ -1,5 +1,7 @@
 package soy.gabimoreno.movies.ui
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import soy.gabimoreno.movies.R
@@ -8,6 +10,11 @@ class NavHostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        val newKeyguardLock = keyguardManager.newKeyguardLock("NavHostActivity")
+        newKeyguardLock.disableKeyguard()
+
         setContentView(R.layout.activity_nav_host)
     }
 }
